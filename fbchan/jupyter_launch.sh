806827fe12c5fc08c0727bb12796cd13ac825baa
Bofeng Chen <bc272@cs.duke.edu>
2023-04-04 15:46:57 -0400

    finetune

---
+++ fbchan/jupyter_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=128GB
+#SBATCH --gres=gpu:1
+#SBATCH --partition=compsci-gpu
+#SBATCH --output=./jupyter_log.log
+
+source /home/users/bc272/anaconda3/bin/activate
+conda activate base_torch
+jupyter-notebook --ip=0.0.0.0 --port=8882 --no-browser
